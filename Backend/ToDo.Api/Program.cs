59590bd462a2cc2e9800f15b724dff2e871dd4d9
RyanDavari <ryan.davari@gmail.com>
2025-11-23 16:14:01 +1100

    Identity, AppUser, JWT settings added

--- Backend/ToDo.Api/Program.cs
+++ Backend/ToDo.Api/Program.cs
@@ -1,10 +1,14 @@
 using AutoMapper;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 using ToDo.Api.Mapping;
 using ToDo.Api.Middlewares;
 
 using ToDo.Api.Services;
 using ToDo.DAL;
+using ToDo.DAL.Models;
 using ToDo.DAL.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -45,6 +49,39 @@ builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(
         builder.Configuration.GetConnectionString("DefaultConnection")));
 
+builder.Services.AddIdentity<AppUser, IdentityRole>(options =>
+{
+    options.Password.RequireDigit = true;
+    options.Password.RequireLowercase = true;
+    options.Password.RequireUppercase = true;
+    options.Password.RequireNonAlphanumeric = true;
+    options.Password.RequiredLength = 12;
+})
+.AddEntityFrameworkStores<AppDbContext>();
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme =
+    options.DefaultChallengeScheme =
+    options.DefaultScheme =
+    options.DefaultSignInScheme =
+    options.DefaultSignOutScheme =
+    JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidIssuer = builder.Configuration["JWT:Issuer"],
+        ValidateAudience = true,
+        ValidAudience = builder.Configuration["JWT:Audience"],
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey (
+            System.Text.Encoding.UTF8.GetBytes(builder.Configuration["JWT:SigningKey"])
+        )
+    };
+});
+
 var app = builder.Build();
 
 // Global exception handling
@@ -65,6 +102,7 @@ app.UseHttpsRedirection();
 
 app.UseCors(FrontendClient);
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 // Map controller routes
---
+++ Backend/ToDo.DAL/Models/AppUser.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace ToDo.DAL.Models;
+
+public class AppUser: IdentityUser
+{
+
+}
